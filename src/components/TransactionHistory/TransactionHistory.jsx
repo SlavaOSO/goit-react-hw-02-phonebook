73c50d38799197062e321068197fd96ee4dc192d
SlavaOSO <98713732+SlavaOSO@users.noreply.github.com>
2022-11-07 19:31:12 +0200

    start

---
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,32 @@
+import PropTypes from 'prop-types';
+import css from './TransactionHistory.module.css'
+
+const TransactionHistory = ({ items }) => {
+  return <table className={css.transactionHistory}>
+    <thead>
+        <tr>
+            <th>Type</th>
+            <th>Amount</th>
+            <th>Currency</th>
+        </tr>
+    </thead>
+
+    <tbody >
+        {items.map(({ id, type, amount, currency }) => (
+            <tr key={id} >
+                <td>{type}</td>
+                <td>{amount}</td>
+                <td>{currency}</td>
+            </tr>))}      
+  
+    </tbody>
+</table>  
+};
+
+TransactionHistory.prototype = {
+    type: PropTypes.string.isRequired,
+    amount: PropTypes.string.isRequired,
+    currency: PropTypes.string.isRequired  
+}
+
+export default TransactionHistory;
